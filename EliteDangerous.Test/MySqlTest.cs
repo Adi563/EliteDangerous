14ee2e92a072932ca2367a71f940f130311e5bbf
Adrian Schatzmann <adimail@hispeed.ch>
2017-05-26 16:23:42 +0200

    Connection globalized

--- EliteDangerous.Test/MySqlTest.cs
+++ EliteDangerous.Test/MySqlTest.cs
@@ -6,8 +6,9 @@
     [TestClass]
     public class MySqlTest
     {
-        [TestMethod]
-        public void ReadSystems()
+        private static MySql.Data.MySqlClient.MySqlConnection connection;
+
+        static MySqlTest()
         {
             var stringBuilder = new MySql.Data.MySqlClient.MySqlConnectionStringBuilder();
             stringBuilder.Server = "192.168.10.157";
@@ -15,21 +16,32 @@
             stringBuilder.Password = "";
             stringBuilder.Database = "elitedangerous";
 
-            using (var connection = new MySql.Data.MySqlClient.MySqlConnection(stringBuilder.ConnectionString))
+            connection = new MySql.Data.MySqlClient.MySqlConnection(stringBuilder.ConnectionString);
+            connection.Open();
+        }
+
+        [TestMethod]
+        public void GetSystems()
+        {
+            ReadSystems().ToArray();
+        }
+
+        public static System.Collections.Generic.IEnumerable<StarSystem> ReadSystems()
+        {
+            var command = connection.CreateCommand();
+            command.CommandText = "SELECT * FROM Systems";
+            command.CommandTimeout = 1000;
+
+            var reader = command.ExecuteReader();
+            while (reader.Read())
             {
-                connection.Open();
+                var id = (uint)reader["Id"];
+                var name = (string)reader["Name"];
+                var x = (float)reader["X"];
+                var y = (float)reader["Y"];
+                var z = (float)reader["Z"];
 
-                var command = connection.CreateCommand();
-                command.CommandText = "SELECT * FROM Systems";
-
-                var reader = command.ExecuteReader();
-                while (reader.Read())
-                {
-                    var name = (string)reader["Name"];
-                    var x = (decimal)reader["X"];
-                    var y = (decimal)reader["Y"];
-                    var z = (decimal)reader["Z"];
-                }
+                yield return new StarSystem { Id = id, Name = name, Location = new System.Numerics.Vector3(x, y, z) };
             }
         }
 
@@ -49,27 +61,16 @@
 
                 return currentGroup;
             }).ToArray();
-
-            var stringBuilder = new MySql.Data.MySqlClient.MySqlConnectionStringBuilder();
-            stringBuilder.Server = "192.168.10.157";
-            stringBuilder.UserID = "root";
-            stringBuilder.Password = "";
-            stringBuilder.Database = "elitedangerous";
-
-            using (var connection = new MySql.Data.MySqlClient.MySqlConnection(stringBuilder.ConnectionString))
+            
+            const string queryPattern = "INSERT INTO Systems VALUES {0};";
+            foreach (var systemGroup in systemsGrouped)
             {
-                connection.Open();
-
-                const string queryPattern = "INSERT INTO Systems VALUES {0};";
-                foreach (var systemGroup in systemsGrouped)
-                {
-                    string queryValues = string.Join(",", systemGroup.Select(system => string.Format("({0},'{1}',{2},{3},{4})", system.Id, system.Name.Replace("\"", string.Empty).Replace("'", "''"), system.Location.X, system.Location.Y, system.Location.Z)));
-                    string query = string.Format(queryPattern, queryValues);
-                    var commandInsert = connection.CreateCommand();
-                    commandInsert.CommandText = query;
-                    commandInsert.CommandTimeout = 1000;
-                    commandInsert.ExecuteNonQuery();
-                }
+                string queryValues = string.Join(",", systemGroup.Select(system => string.Format("({0},'{1}',{2},{3},{4})", system.Id, system.Name.Replace("\"", string.Empty).Replace("'", "''"), system.Location.X, system.Location.Y, system.Location.Z)));
+                string query = string.Format(queryPattern, queryValues);
+                var commandInsert = connection.CreateCommand();
+                commandInsert.CommandText = query;
+                commandInsert.CommandTimeout = 1000;
+                commandInsert.ExecuteNonQuery();
             }
         }
 
@@ -78,34 +79,23 @@
         {
             var systems = ReadCsvTest.GetAllSystems();
 
-            var stringBuilder = new MySql.Data.MySqlClient.MySqlConnectionStringBuilder();
-            stringBuilder.Server = "192.168.10.157";
-            stringBuilder.UserID = "root";
-            stringBuilder.Password = "";
-            stringBuilder.Database = "elitedangerous";
-
-            using (var connection = new MySql.Data.MySqlClient.MySqlConnection(stringBuilder.ConnectionString))
+            foreach (var system in systems)
             {
-                connection.Open();
+                var command = connection.CreateCommand();
+                command.CommandText = string.Format("SELECT COUNT(*) FROM Systems WHERE Id = {0}", system.Id);
+                var numberOfRows = (long)command.ExecuteScalar();
 
-                foreach (var system in systems)
+                if (numberOfRows > 0)
                 {
-                    var command = connection.CreateCommand();
-                    command.CommandText = string.Format("SELECT COUNT(*) FROM Systems WHERE Id = {0}", system.Id);
-                    var numberOfRows = (long)command.ExecuteScalar();
-
-                    if (numberOfRows > 0)
-                    {
-                        var commandUpdate = connection.CreateCommand();
-                        commandUpdate.CommandText = string.Format("UPDATE Systems SET Name = '{0}', X = {1}, Y = {2}, Z = {3} WHERE Id = {4}", system.Name.Replace("\"", string.Empty).Replace("'", "''"), system.Location.X, system.Location.Y, system.Location.Z, system.Id);
-                        commandUpdate.ExecuteNonQuery();
-                    }
-                    else
-                    {
-                        var commandInsert = connection.CreateCommand();
-                        commandInsert.CommandText = string.Format("INSERT INTO Systems (Id, Name, X, Y, Z) VALUES ({0}, '{1}', {2}, {3}, {4})", system.Id, system.Name.Replace("\"", string.Empty).Replace("'", "''"), system.Location.X, system.Location.Y, system.Location.Z);
-                        commandInsert.ExecuteNonQuery();
-                    }
+                    var commandUpdate = connection.CreateCommand();
+                    commandUpdate.CommandText = string.Format("UPDATE Systems SET Name = '{0}', X = {1}, Y = {2}, Z = {3} WHERE Id = {4}", system.Name.Replace("\"", string.Empty).Replace("'", "''"), system.Location.X, system.Location.Y, system.Location.Z, system.Id);
+                    commandUpdate.ExecuteNonQuery();
+                }
+                else
+                {
+                    var commandInsert = connection.CreateCommand();
+                    commandInsert.CommandText = string.Format("INSERT INTO Systems (Id, Name, X, Y, Z) VALUES ({0}, '{1}', {2}, {3}, {4})", system.Id, system.Name.Replace("\"", string.Empty).Replace("'", "''"), system.Location.X, system.Location.Y, system.Location.Z);
+                    commandInsert.ExecuteNonQuery();
                 }
             }
         }
@@ -113,22 +103,9 @@
         [TestMethod]
         public void DeleteSystems()
         {
-            var systems = ReadCsvTest.GetAllSystems();
-
-            var stringBuilder = new MySql.Data.MySqlClient.MySqlConnectionStringBuilder();
-            stringBuilder.Server = "192.168.10.157";
-            stringBuilder.UserID = "root";
-            stringBuilder.Password = "";
-            stringBuilder.Database = "elitedangerous";
-
-            using (var connection = new MySql.Data.MySqlClient.MySqlConnection(stringBuilder.ConnectionString))
-            {
-                connection.Open();
-
-                var commandUpdate = connection.CreateCommand();
-                commandUpdate.CommandText = "TRUNCATE TABLE Systems";
-                commandUpdate.ExecuteNonQuery();
-            }
+            var commandUpdate = connection.CreateCommand();
+            commandUpdate.CommandText = "TRUNCATE TABLE Systems";
+            commandUpdate.ExecuteNonQuery();
         }
     }
 }
